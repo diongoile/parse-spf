239dce26161a8547f9089e04b2393f30476c2ec0
Dion Goile <dion@gcits.com>
2019-11-15 15:07:10 +1300

    NPM Init

--- parse-spf.js
+++ parse-spf.js
@@ -1,5 +1,3 @@
-const testingRecord = ""
-
 const ParseSpf = (record) => {
     const data = []
     const reges = [/include:/i, /ip4:/i, /ip6:/i, /a:/i, /mx:/i, /ptr/i, /exists/i, /[-,+,?,~]all/i, /v=spf1/i]
@@ -23,19 +21,18 @@ const ParseSpf = (record) => {
                         data.push({ type: 'policy', content: match[0], notes: "Policy type: SoftFail" });
                         break;
                     case "v=spf1":
-                        data.push({ type: 'policy', content: match[0], notes: "SPF Version 1" });
+                        data.push({ type: 'version', content: match[0], notes: "SPF Version 1" });
                         break;
                     default:
                         const string = component.split(match)
                         data.push({ type: match[0], content: string[1] })
                         break
                 }
-
-
             }
         });
     }
     components.map(component => mapComponent(component, reges))
-    console.log(data)
+    return data
 }
-ParseSpf(testingRecord)
+
+export default ParseSpf;
